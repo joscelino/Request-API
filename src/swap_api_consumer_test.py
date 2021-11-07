e5ab1ed20665e2cd9ce88e84c3f808cedca9ce37
Joscelino Oliveira <joscelino.oliveira@gmail.com>
2021-11-07 16:11:17 -0300

    Primeiro mock de testes de requisicao.

--- src/swap_api_consumer_test.py
+++ src/swap_api_consumer_test.py
@@ -1,8 +1,11 @@
 from .swap_api_consumer import SwapApiConsumer
 
 
-def test_get_starships():
+def test_get_starships(requests_mock):
     """Testing get_startships method"""
+    requests_mock.get(
+        "https://swapi.dev/api/starships/", status_code=200, json={"some": "thing"}
+    )
     swap_api_consumer = SwapApiConsumer()
     response = swap_api_consumer.get_starships(page=1)
     print(response)
