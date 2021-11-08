6957566d83b4a062d3fe36b8c568f05fabb04355
Joscelino Oliveira <joscelino.oliveira@gmail.com>
2021-11-07 21:07:11 -0300

    Refatorando o script de consumo da API e testes.

--- src/swap_api_consumer_test.py
+++ src/swap_api_consumer_test.py
@@ -1,11 +1,17 @@
 from .swap_api_consumer import SwapApiConsumer
 
 
-def test_get_starships(requests_mock):
+def test_get_starships():
     """Testing get_startships method"""
-    requests_mock.get(
-        "https://swapi.dev/api/starships/", status_code=200, json={"some": "thing"}
-    )
+    # requests_mock.get(
+    #     "https://swapi.dev/api/starships/", status_code=200, json={"some": "thing"}
+    # )
     swap_api_consumer = SwapApiConsumer()
-    response = swap_api_consumer.get_starships(page=1)
-    print(response)
+    page = 1
+    get_starships_response = swap_api_consumer.get_starships(page=page)
+
+    assert get_starships_response.request.method == "GET"
+    assert get_starships_response.request.url == "https://swapi.dev/api/starships/"
+    assert get_starships_response.request.params == {"page": page}
+    assert get_starships_response.status_code == 200
+    assert isinstance(get_starships_response.response["results"], list)
